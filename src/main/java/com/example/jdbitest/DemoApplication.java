dc0d27fed56c5e74616a0a8baa57fbafae045511
karintomania <19652340+karintomania@users.noreply.github.com>
2020-03-18 22:38:49 +0000

    first commit

---
+++ src/main/java/com/example/jdbitest/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
